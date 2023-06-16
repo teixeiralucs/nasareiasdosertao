d598a44ff25eeb252d1584927989cef0d32f7e55
LucasTeixeira <tlucasmatheus@gmail.com>
2023-06-16 16:04:42 -0300

    VSC Commit and Push

--- src/Main.java
+++ src/Main.java
@@ -19,14 +19,38 @@ public class Main {
                              "um jeito de viver. Numa cidade pequena, no sertão brasileiro,\n" +
                              "um herói tem que escolher entre amar e lutar.\n" +
                              "Bem-vindo ao Nas Areias dos Sertões");
+            System.out.println("Nessa sessão vamos escolher alguns aspectos o seu personagem" +
+                                " tais como nome, genero e arma favorita");
+            System.out.println("Agora me fala, seu personagem é HOMEM ou MULHER?");
+            String homem = ("HOMEM");
+            String genero = leitor .nextLine();
+
+            if (genero.equalsIgnoreCase(homem)){
+
+            }
+            else{
+
+            }                                    
         }
         else{
             System.out.println("In a wasteland future, the potable water of the planet has gone\n" +
 	                        "the polar ice caps melted and the ocean drowned most of the cities,\n" +
                             "killed billions and forced the left citizen to search in the countryside\n" +
-                            "a way to live. In a small city, in Brazilians Sertão,\n" +
+                            "a way to live. In a small city, in Brazilian's Sertão,\n" +
                             "a hero has to choose between love and fight.\n" +
-                            "Welcome to In Sertãos Sands");
+                            "Welcome to In Sertão's Sands");
+            System.out.println("“In this section we're going to choose a few aspects\n" +
+                            "of your character, as name, gender and favorite weapon. Let 's start.");
+            System.out.println("Now tell me, your character is a MAN or a WOMAN?");
+            String man = ("MAN");
+            String gender = leitor .nextLine();
+
+            if (gender.equalsIgnoreCase(man)){
+
+            }
+            else{
+                
+            }
         }
         leitor.close();
     }
